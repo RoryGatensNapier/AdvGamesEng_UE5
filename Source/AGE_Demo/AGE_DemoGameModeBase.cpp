de8bd69efd589e44ec391102d3ebfa00a78ed1a6
Axaerus <rgater08@gmail.com>
2022-10-25 23:29:09 +0100

    Initial commit
    Initial commit

---
+++ Source/AGE_Demo/AGE_DemoGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "AGE_DemoGameModeBase.h"
+
---
+++ Source/AGE_Demo/AGE_DemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AGE_DemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGE_DEMO_API AAGE_DemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
